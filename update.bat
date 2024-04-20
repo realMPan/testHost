0b6f3d094b3fc97d390419b131b19eaa10b69966
realMPan <123257843+realMPan@users.noreply.github.com>
2024-04-20 15:05:46 -0400

    A lot of changes

--- update.bat
+++ update.bat
@@ -2,4 +2,4 @@
 echo Publish
 call gradlew.bat publish
 echo Copy
-Xcopy /E /y .\\build\\repos\\releases\\com\\testHost .\\testHost\\repos\\releases\\com\\testHost
+Xcopy /E /y .\\build\\repos\\releases\\com\\testHost .\\bingus\\repos\\releases\\com\\testHost
